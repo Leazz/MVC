bdc1e5d7eaea6c80a82a2f1ace8d5bf205926a7f
Lea Alfawaz <halaef12@gmail.com>
2023-05-01 03:20:21 +0200

    fixing files

--- public/symfony/app/src/Controller/LuckyControllerTwig.php
+++ public/symfony/app/src/Controller/LuckyControllerTwig.php
@@ -31,4 +31,43 @@ class LuckyControllerTwig extends AbstractController
     {
         return $this->render('about.html.twig');
     }
+
+    #[Route('/', name: 'me')]
+    public function me(): Response
+    {
+        return $this->render('me.html.twig', [
+            'name' => 'Lea',
+            'image' => 'me.jpg',
+            'description' => 'A little about me '
+        ]);
+    }
+
+    #[Route('/api/quote')]
+    public function quote(): Response
+    {
+        $quotes = [
+            'You can do it, you are strong <3',
+            'Love your self',
+            'Success dosent come to you, you go yo it',
+        ];
+
+        $randomQuote = $quotes[array_rand($quotes)];
+
+        $data = [
+            'quote' => $randomQuote,
+            'date' => date('Y-m-d'),
+            'timestamp' => time(),
+        ];
+        $response = new Response();
+        $response->setContent(json_encode($data));
+
+        return $response;
+    }
+
+    #[Route('/report')]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
 }
